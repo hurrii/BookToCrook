ac43a5d1b56609aaa0b3907fb6cf5d79d64ce0e6
hurrii <hurrii.ftw@gmail.com>
2019-12-26 01:30:44 +0300

    сделать header fixed

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -18,6 +18,11 @@ export default {
 
 <style lang="stylus" scoped>
   .header
+    position fixed
+    top 0
+    left 0
+    right 0
+    z-index 900
     height $headerHeightDesktop
     background $green
     box-shadow 0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12)
