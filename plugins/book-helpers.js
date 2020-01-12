2185e7deb67b0cbaa050aacb04a2652e3677dcd6
hurrii <hurrii.ftw@gmail.com>
2020-01-12 08:36:49 +0300

    убрать case sensitivity у поиска

--- plugins/book-helpers.js
+++ plugins/book-helpers.js
@@ -1,11 +1,11 @@
 export function hasAuthor(book, query) {
-    return book.volumeInfo.authors.join('').toLowerCase().includes(query)
+    return book.volumeInfo.authors.join('').toLowerCase().includes(query.toLowerCase())
 }
 
 export function hasTitle(book, query) {
-  return book.volumeInfo.title.toLowerCase().includes(query)
+  return book.volumeInfo.title.toLowerCase().includes(query.toLowerCase())
 }
 
 export function hasCategory(book, query) {
-  return book.volumeInfo.infoLink.includes(query) || book.volumeInfo.previewLink.includes(query)
+  return book.volumeInfo.infoLink.includes(query.toLowerCase()) || book.volumeInfo.previewLink.includes(query.toLowerCase())
 }
